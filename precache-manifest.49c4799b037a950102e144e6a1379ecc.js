b90a98834f065fd9c620383b3fd023a99e3dfd00
medvecky <alexey.medvecky@gmail.com>
2020-04-07 10:24:37 +0300

    Updates

---
+++ precache-manifest.49c4799b037a950102e144e6a1379ecc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb9e89ff2f388ba0d388aea8e1bcdad",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "349bb2559f32787a431d",
+    "url": "/javascript-calculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c871f10ea1404593a2cb",
+    "url": "/javascript-calculator/static/css/main.b4b84771.chunk.css"
+  },
+  {
+    "revision": "349bb2559f32787a431d",
+    "url": "/javascript-calculator/static/js/2.0cfb5d81.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/javascript-calculator/static/js/2.0cfb5d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c871f10ea1404593a2cb",
+    "url": "/javascript-calculator/static/js/main.5c83fe34.chunk.js"
+  },
+  {
+    "revision": "1dd0b5f85af98b5c317b",
+    "url": "/javascript-calculator/static/js/runtime-main.ca8e9524.js"
+  }
+]);
--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import calculatorReducer from '../features/calculator/calculatorSlice';
-
-export default configureStore({
-    reducer: {
-        calculator: calculatorReducer,
-    },
-});
